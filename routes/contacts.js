5bf90565963c39379fbe5cebf828f00b6430f9c0
Ajay Manikanta <ajay.vijay1123@gmail.com>
2021-08-25 17:11:08 +0530

    Enhancement: add delete route for contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -60,49 +60,83 @@ router.post(
 // @route     PUT api/contacts
 // @desc      Update existing contact
 // @access    Private
-router.put('/:id', [authMiddleware], async (req, res) => {
-  const { name, phone, email, type } = req.body;
+router.put(
+  '/:id',
+  [
+    authMiddleware,
+    [
+      body('name', 'Please add a name').not().isEmpty(),
+      body('email', 'Please enter valid email').isEmail(),
+      body('phone', 'Please enter valid 10 digit phone number').isLength(10),
+    ],
+  ],
+  async (req, res) => {
+    // Validate the request body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors);
+    }
 
-  // Build contact object
-  const contactFields = {};
-  if (name) contactFields.name = name;
-  if (phone) contactFields.phone = phone;
-  if (email) contactFields.email = email;
-  if (type) contactFields.type = type;
+    const { name, phone, email, type } = req.body;
 
+    // Build contact object
+    const contactFields = {};
+    if (name) contactFields.name = name;
+    if (phone) contactFields.phone = phone;
+    if (email) contactFields.email = email;
+    if (type) contactFields.type = type;
+
+    try {
+      let contact = await Contact.findById(req.params.id);
+      if (!contact)
+        return res
+          .status(404)
+          .json({ statusCode: 404, msg: 'Contact not found...' });
+
+      // Make sure user owns contact
+      if (contact.user.toString() !== req.user.id)
+        return res.status(401).json({ statusCode: 401, msg: 'Not authorized' });
+
+      contact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: contactFields,
+        },
+        { new: true }
+      );
+
+      res.status(200).json({
+        statusCode: 200,
+        msg: `contact updated successfully... ${contact}`,
+      });
+    } catch (error) {
+      res.status(500).json({ statusCode: 500, msg: error.message });
+    }
+  }
+);
+
+// @route     DELETE api/contacts
+// @desc      Delete existing contact
+// @access    Private
+router.delete('/:id', authMiddleware, async (req, res) => {
   try {
     let contact = await Contact.findById(req.params.id);
     if (!contact)
       return res
         .status(404)
-        .json({ statusCode: 404, msg: 'Contact not found...' });
+        .json({ statusCode: 404, msg: 'No such contact exists...' });
 
-    // Make sure user owns contact
     if (contact.user.toString() !== req.user.id)
-      return res.status(401).json({ statusCode: 401, msg: 'Not authorized' });
+      return res
+        .status(401)
+        .json({ statusCode: 401, msg: 'User unauthorised' });
 
-    contact = await Contact.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: contactFields,
-      },
-      { new: true }
-    );
+    contact = await Contact.findByIdAndDelete(req.params.id);
 
-    res.status(200).json({
-      statusCode: 200,
-      msg: `contact updated successfully... ${contact}`,
-    });
-  } catch (error) {
-    res.status(500).json({ statusCode: 500, msg: error.message });
+    res.status(200).json({ statusCode: 200, msg: contact });
+  } catch (err) {
+    res.status(500).json({ statusCode: 500, msg: err.message });
   }
-});
-
-// @route     DELETE api/contacts
-// @desc      Delete existing contact
-// @access    Private
-router.delete('/:id', (req, res) => {
-  res.send('delete contact..');
 });
 
 module.exports = router;
