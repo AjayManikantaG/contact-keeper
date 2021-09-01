63ba4c6bc6c03f52529732489b09bd47b8b8e1ba
Ajay Manikanta <ajay.vijay1123@gmail.com>
2021-09-02 00:16:31 +0530

    add home and about component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
