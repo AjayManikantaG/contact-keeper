f649b9de1c94ef36bad29d5bd1e61139e512590e
Ajay Manikanta <ajay.vijay1123@gmail.com>
2021-09-01 23:58:45 +0530

    add css to the main app

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Navbar = () => {
-  return ()
-}
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <i className={icon}></i>
+        {title}
+      </h1>
+    </div>
+  );
+};
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired
-}
+  icon: PropTypes.string.isRequired,
+};
 
 Navbar.defaultProps = {
   title: 'Contact Keeper',
-  icon: 'fas fa-id-card-alt'
-}
+  icon: 'fas fa-id-card-alt',
+};
 
 export default Navbar;
