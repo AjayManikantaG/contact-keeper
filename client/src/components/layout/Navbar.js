6b4bc5cbef6fbf0c526809ae073de4c69530f5c0
Ajay Manikanta <ajay.vijay1123@gmail.com>
2021-09-01 23:56:00 +0530

    add proptypes and default props

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,3 +4,15 @@ import PropTypes from 'prop-types';
 const Navbar = () => {
   return ()
 }
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+}
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt'
+}
+
+export default Navbar;
