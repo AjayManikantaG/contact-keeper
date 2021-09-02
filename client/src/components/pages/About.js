f9e3272c1eda35a5446aa78345037740c6842c91
Ajay Manikanta <ajay.vijay1123@gmail.com>
2021-09-02 23:59:09 +0530

    add home and about components

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './About.css';
 
 const About = () => {
+  useEffect(() => {}, []);
+
   return (
-    <div>
-      <h1>About Component</h1>
+    <div className={animateUp}>
+      <h1>About This App</h1>
+      <p className='my-1'>This is a contact keeper app which stores contacts</p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong> 1.0.0
+      </p>
     </div>
   );
 };
